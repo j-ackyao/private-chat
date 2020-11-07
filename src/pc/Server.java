843d3b7f54318d0daf20f517dbf07d987254c93d
eee <unconfigured@null.spigotmc.org>
2020-11-07 11:53:03 -0800

    easier to open server

--- src/pc/Server.java
+++ src/pc/Server.java
@@ -1,6 +1,8 @@
 package pc;
 
 import java.io.BufferedReader;
+import java.io.Console;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -18,6 +20,15 @@ public class Server extends Thread {
 
 	public static void main(String[] args) {
 
+		Console con = System.console();
+		if(con == null) {
+			try {
+				String launcherName = new File(Launcher.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getName();
+				Runtime.getRuntime().exec(new String[] {"cmd", "/c", "start", "cmd", "/k", "java -jar " + launcherName});
+				System.exit(0);
+			} catch (IOException e) {}
+		}
+		
 		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
 		connectedClients = new ArrayList<Socket>();
